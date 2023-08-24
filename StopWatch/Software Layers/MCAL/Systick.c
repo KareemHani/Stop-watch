c80b4eb7af123b9e48b2c59b59727b6da0ceec7f
KareemHani <102494852+KareemHani@users.noreply.github.com>
2023-08-24 23:35:51 +0300

    Stop watch

---
+++ StopWatch/Software Layers/MCAL/Systick.c
@@ -0,0 +1,78 @@
+/*
+ * Systick.c
+ *
+ *  Created on: Aug 24, 2023
+ *      Author: EUI-Support
+ */
+#include "Systick.h"
+#include "BITWISE_OPERATIONS.h"
+
+void Systick_Init()
+{
+    // Enabling the System Clock
+    SETBIT(NVIC_ST_CTRL_R,2);
+
+    // Enabling Systick Interrupt
+    SETBIT(NVIC_ST_CTRL_R,1);
+
+    // Systick Enable bit
+    SETBIT(NVIC_ST_CTRL_R,0);
+
+    // Placing a value of 1 Second in the reload register
+    NVIC_ST_RELOAD_R=0x00F42400-1;
+}
+
+void display()
+{
+   static int counter=0;
+   //static int sec=0;
+   char Save_string[50]={};
+
+
+
+    // Incrementing the counter
+    counter++;
+
+    // Convert integer to string
+    ltoa(counter,Save_string);
+
+    // Printing the string on the LCD
+    SEND_STRING(Save_string);
+
+
+
+
+}
+
+void Stop()
+{
+
+    CLEARBIT(NVIC_ST_CTRL_R,0);
+
+}
+
+
+/*  // In case we are using polling
+void display()
+{
+   static int counter=0;
+   static int sec=0;
+   char Save_string[50]={};
+
+   //Checks if the count flag is equal to 1
+   if(CHECKBIT(NVIC_ST_CTRL_R,16)==1)
+   {
+       // Incrementing the counter
+       counter++;
+
+       // Convert integer to string
+       ltoa(counter,Save_string);
+
+       // Printing the string on the LCD
+       SEND_STRING(Save_string);
+
+   }
+
+
+}
+*/
---
+++ StopWatch/Software Layers/MCAL/Systick.h
@@ -0,0 +1,18 @@
+/*
+ * Systick.h
+ *
+ *  Created on: Aug 24, 2023
+ *      Author: EUI-Support
+ */
+#include "LCD.h"
+#include "GPIO_INTERRUPT.h"
+
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+void Systick_Init();
+void display();
+
+
+#endif /* SYSTICK_H_ */
